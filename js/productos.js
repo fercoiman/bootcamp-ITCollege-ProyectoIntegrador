70b456b5b3f512cb56b74c9ee773d215f2870719
fercoiman <fcoiman@frba.utn.edu.ar>
2025-08-25 19:26:39 -0300

    first commit

---
+++ js/productos.js
@@ -0,0 +1,10 @@
+// ------------------------------------------------
+//             variables globales
+// ------------------------------------------------
+var productos = [
+    { nombre: 'TV',             precio: 12345,      stock: 44,      marca: 'Samsung',       categoria: 'Computación',       detalles: 'LCD 70 Pulgadas',    foto: 'https://images.samsung.com/is/image/samsung/latin_LN40C650L1FXZA_001_Front?$624_624_PNG$',   envio: false },
+    { nombre: 'Mouse',          precio: 23456,      stock: 55,      marca: 'Genius',        categoria: 'Computación',       detalles: 'Color negro',        foto: 'https://www.comeros.com.ar/wp-content/uploads/2019/05/medium_1-1.jpg',                       envio: true },
+    { nombre: 'Mesa',           precio: 34567,      stock: 66,      marca: 'Pinolandia',    categoria: 'Hogar',             detalles: 'Madera de Pino',     foto: 'https://acdn-us.mitiendanube.com/stores/912/364/products/mesa-de-pino-rosario-120-cm1-53021f0db2801ed53a16667053545497-640-0.jpg',   envio: false },
+    { nombre: 'Heladera',       precio: 45678,      stock: 77,      marca: 'Philips',       categoria: 'Electrodoméstico',  detalles: 'No Frost',           foto: 'https://http2.mlstatic.com/D_NQ_NP_780142-MLA83944984827_042025-O.webp',                     envio: true },
+    { nombre: 'Memoria DDR',    precio: 56789,      stock: 88,      marca: 'Kingston',      categoria: 'Computación',       detalles: '32GB',               foto: 'https://http2.mlstatic.com/D_NQ_NP_941896-MEC50757154629_072022-O.webp',                     envio: false },
+]
